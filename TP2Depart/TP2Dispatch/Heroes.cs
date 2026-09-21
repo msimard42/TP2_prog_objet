9c9479d429b085930659d3c1f15f88486262a7a4
Michael Simard <simmic5@hotmail.com>
2026-09-21 09:20:10 -0400

    initial push with class creation

---
+++ TP2Depart/TP2Dispatch/Heroes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP2Dispatch
+{
+	public class Heroes
+	{
+		private string _name;
+		private int _level;
+		private int _restRemaining;
+		private Dictionary<Event, bool> _history;
+		private Dictionary<string, int> _stats;
+	}
+}
