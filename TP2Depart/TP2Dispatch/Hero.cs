cb302236d76b4c80352810699a554614778700a9
Michael Simard <simmic5@hotmail.com>
2026-09-23 09:31:37 -0400

    Incomplete Stats class

---
+++ TP2Depart/TP2Dispatch/Hero.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace TP2Dispatch
+{
+	public class Hero
+	{
+		private string _name;
+		private int _level;
+		private int _restRemaining;
+		private Stats _playerStats;
+		private Dictionary<Event, bool> _history;
+
+		public string Name
+		{
+			get => _name;
+			private set
+			{
+				_name = value;
+			}
+		}
+
+		public int Level
+		{
+			get => _level;
+			private set
+			{
+				_level = value;
+			}
+		}
+
+		public int RestRemaining
+		{
+			get => _restRemaining;
+			private set
+			{
+				_restRemaining = value;
+			}
+		}
+
+		public Stats PlayerStats
+		{
+			get => _playerStats;
+			private set
+			{
+				_playerStats = value;
+			}
+		}
+
+		public Dictionary<Event, bool> History
+		{
+			get => _history;
+			private set
+			{
+				_history = value;
+			}
+		}
+
+		//public Heroes(string name, 
+	}
+}
---
+++ TP2Depart/TP2Dispatch/Stats.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP2Dispatch
+{
+	public class Stats
+	{
+		const int BASE_STAT_VALUE = 1;
+		private string[] _statsName;
+		private int _mobility;
+		private int _vigor;
+		private int _intelligence;
+		private int _charisma;
+		private Dictionary<string, int> _statsList;
+
+		public string[] StatsName
+		{
+			get => _statsName;
+			private set
+			{
+				_statsName = value;
+			}
+		}
+
+		public int Mobility
+		{
+			get => _mobility;
+			private set
+			{
+				if (value < 1)
+					_mobility = BASE_STAT_VALUE;
+				else
+					_mobility = value;
+			}
+		}
+
+		public int Vigor
+		{
+			get => _vigor;
+			private set
+			{
+				if (value < 1)
+					_vigor = BASE_STAT_VALUE;
+				else
+					_vigor = value;
+			}
+		}
+
+		public int Intelligence
+		{
+			get => _intelligence;
+			private set
+			{
+				if (value < 1)
+					_intelligence = BASE_STAT_VALUE;
+				else
+					_intelligence = value;
+			}
+		}
+
+		public int Charisma
+		{
+			get => _charisma;
+			private set
+			{
+				if (value < 1)
+					_charisma = BASE_STAT_VALUE;
+				else
+					_charisma = value;
+			}
+		}
+
+		public Dictionary<string, int> StatsList
+		{
+			get => _statsList;
+			private set
+			{
+				_statsList = value;
+			}
+		}
+
+		public Stats()
+		{
+			StatsName = [ "Mobility", "Vigor", "Intelligence", "Charisma" ];
+			Mobility = BASE_STAT_VALUE;
+			Vigor = BASE_STAT_VALUE;
+			Intelligence = BASE_STAT_VALUE;
+			Charisma = BASE_STAT_VALUE;
+			StatsList = new Dictionary<string, int>();
+			for (int i = 0; i < StatsList.Count; i++)
+			{
+				StatsList[StatsName[i]] = BASE_STAT_VALUE;
+			}
+		}
+
+		public Stats(int mobility, int vigor, int intelligence, int charisma)
+		{
+			this.Mobility = mobility;
+			this.StatsList[nameof(mobility)] = mobility;
+			this.Vigor = vigor;
+			this.StatsList[nameof(vigor)] = vigor;
+			this.Intelligence = intelligence;
+			this.StatsList[nameof(intelligence)] = intelligence;
+			this.Charisma = charisma;
+			this.StatsList[nameof(charisma)] = charisma;
+		}
+
+		public void IncreaseRandomStats(int amountToSplit)
+		{
+			//to do
+		}
+	}
+}
